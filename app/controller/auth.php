8d8d88f561428044b8f49341aa12d8ef0c222e40
koustav96 <koustav.mondal@innoraft.com>
2024-04-24 17:16:03 +0530

    FT2-46: More comments added.

--- app/controller/auth.php
+++ app/controller/auth.php
@@ -5,16 +5,18 @@ require_once __DIR__. '/login_control/config.php';
 require_once __DIR__.'/../model/query.php';
 
 use GuzzleHttp\Client;
-
+// Create a new Guzzle HTTP client.
 $client = new Client();
-
+// Split query string parameters and extract authorization code.
 $param_arr = explode('&', $query);
 $code_arr = explode('=', $param_arr[0]);
 $code = $code_arr[1];
 
+// Check if authorization code is set.
 if (isset($code)) {
   $acc_url = "https://www.linkedin.com/oauth/v2/accessToken";
   try {
+    // Request access token using authorization code.
     $response = $client->request('POST', $acc_url, [
       'form_params' => [
         'grant_type' => 'authorization_code',
@@ -24,8 +26,10 @@ if (isset($code)) {
         'redirect_uri' => $redirect_uri
       ]
     ]);
+    // Decode the response and extract ID token.
     $arr = json_decode($response->getBody(), true);
     $id_token = $arr['id_token'];
+    // Extract payload from ID token.
     try {
       $ex_arr = explode('.', $id_token);
       $payload_encrypted = $ex_arr[1];
@@ -33,15 +37,18 @@ if (isset($code)) {
       $email = $payload['email'];
       $name = $payload['name'];
 
+      // Start session and perform user check.
       session_start();
       $dbQueries = new Queries();
       $dbQueries->checkUser($email);
+      // If user exists, set session variables and redirect to homepage.
       if ($dbQueries->checkUser($email)) {
         $_SESSION['data'] = true;
         $_SESSION['email'] = $email;
         $_SESSION['name'] = $name;
         header('location: /home');
       } 
+      // If user does not exist, insert user into database and redirect to homepage.
       else {
         $_SESSION['email'] = $email;
         $_SESSION['name'] = $name;
@@ -49,9 +56,11 @@ if (isset($code)) {
         $dbQueries->insertUser($name, $email, NULL);
         header('location:/home');
       }
+      // Catch any exceptions during payload extraction.
     } catch (Exception $e) {
       echo $e->getMessage();
     }
+    // Catch any exceptions during access token request.
   } catch (Exception $e) {
     echo $e->getMessage();
   }
